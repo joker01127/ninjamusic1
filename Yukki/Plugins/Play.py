6abe37830ba7bc03c2316df9f26cd13fefad79c5
NiNja Vs ALL <81846001+ninja1120@users.noreply.github.com>
2022-03-01 05:15:30 +0200

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -41,7 +41,7 @@ loop = asyncio.get_event_loop()
 
 
 @app.on_message(
-    filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group
+    filters.command(["play", "شغل" f"play@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
